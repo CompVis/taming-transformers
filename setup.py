e30e8fbc5b82a76ec66ae57e822887efa40c005c
Ben Firshman <ben@firshman.co.uk>
2021-06-07 11:41:53 -0700

    Use find_namespace_packages() in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='taming-transformers',
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["taming", "taming.*"]),
     install_requires=[
         'torch',
         'numpy',
