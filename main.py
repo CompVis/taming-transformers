aeabaa34974f5db31e5f61d2c479612e80b4175f
Joe Halliwell <joe.halliwell@gmail.com>
2021-07-10 08:40:55 +0100

    Move get_obj_from_str() and instantiate_from_config() into package

--- main.py
+++ main.py
@@ -11,15 +11,8 @@ from pytorch_lightning.trainer import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, Callback, LearningRateMonitor
 from pytorch_lightning.utilities.distributed import rank_zero_only
 
+from taming import get_obj_from_str, instantiate_from_config
 from taming.data.utils import custom_collate
-
-
-def get_obj_from_str(string, reload=False):
-    module, cls = string.rsplit(".", 1)
-    if reload:
-        module_imp = importlib.import_module(module)
-        importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=None), cls)
 
 
 def get_parser(**parser_kwargs):
---
+++ taming/__init__.py
@@ -0,0 +1,13 @@
+import importlib
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+def instantiate_from_config(config):
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
