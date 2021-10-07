5f86b5cb7ba0b781527a1d42aacd2552654c45e5
Chris Callison-Burch <ccb@stanfordalumni.org>
2021-10-07 14:26:41 -0400

    Update helper_types.py

--- taming/data/helper_types.py
+++ taming/data/helper_types.py
@@ -1,4 +1,5 @@
-from typing import Dict, Tuple, Literal, Optional, NamedTuple, Union
+from typing import Dict, Tuple, Optional, NamedTuple, Union
+from typing_extensions import Literal
 
 from PIL.Image import Image as pil_image
 from torch import Tensor
