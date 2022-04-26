15e55ac5e67c7be9fe0c788e6e391faad1a25a58
rdudhat_umass_edu <rdudhat_umass_edu@ials-gpu018.unity.rc.umass.edu>
2022-04-26 20:14:37 +0000

    Adding the script to resize the images, and some changes to original implementation

--- main.py
+++ main.py
@@ -535,7 +535,7 @@ if __name__ == "__main__":
         # configure learning rate
         bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
         if not cpu:
-            ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
+            ngpu = lightning_config.trainer.gpus
         else:
             ngpu = 1
         accumulate_grad_batches = lightning_config.trainer.accumulate_grad_batches or 1
