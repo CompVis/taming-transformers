3b4ca336be45742c852b31e20b6d82be4b32f5fd
O. Inha <50331907+olaviinha@users.noreply.github.com>
2023-06-13 14:29:03 +0300

    Drop nonexisting module torch._six
    Fix `ModuleNotFoundError: No module named 'torch._six'`

--- taming/data/utils.py
+++ taming/data/utils.py
@@ -8,10 +8,10 @@ from pathlib import Path
 import numpy as np
 import torch
 from taming.data.helper_types import Annotation
-from torch._six import string_classes
 from torch.utils.data._utils.collate import np_str_obj_array_pattern, default_collate_err_msg_format
 from tqdm import tqdm
 
+string_classes = str
 
 def unpack(path):
     if path.endswith("tar.gz"):
