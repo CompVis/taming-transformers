179f7aeb40e761a934fc9b7a2605ae93da6e9b90
Robin Rombach <robin.rombach@gmail.com>
2023-06-28 15:46:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='taming-transformers',
+    name='taming',
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
